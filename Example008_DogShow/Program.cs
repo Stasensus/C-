501b60faacd456501ad3dcaa5e4e4c5466c663f3
Stasensus <stanislav@kireev.ru>
2022-08-05 13:54:33 +0300

    Написал не работает

---
+++ Example008_DogShow/Program.cs
@@ -0,0 +1,31 @@
+﻿int count = 0;
+int distance = 10000;
+int firstfriendspeed = 1;
+int secondfriendspeed = 2;
+int dogspeed = 5;
+int friend = 2;
+int time = 0;
+
+while(distance < 10000)
+{
+    
+    if (friend == 1)
+    {
+        time = distance / (firstfriendspeed + dogspeed);
+        friend = 2;
+        distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+        count = count + 1;
+    }
+    if (friend == 2)
+    {
+        time = distance / (secondfriendspeed + dogspeed);
+        friend = 1;
+        distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+        count = count + 1;
+    }
+    
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
