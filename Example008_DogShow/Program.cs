236b57852e72bd14723ee8d6dc01e79b12f3a31b
Stasensus <stanislav@kireev.ru>
2022-08-06 17:49:56 +0300

    программа про собаку

--- Example008_DogShow/Program.cs
+++ Example008_DogShow/Program.cs
@@ -6,24 +6,21 @@ int dogspeed = 5;
 int friend = 2;
 int time = 0;
 
-while(distance < 10000)
+while(distance > 10)
 {
     
     if (friend == 1)
     {
         time = distance / (firstfriendspeed + dogspeed);
         friend = 2;
-        distance = distance - (firstfriendspeed + secondfriendspeed) * time;
-        count = count + 1;
     }
     if (friend == 2)
     {
         time = distance / (secondfriendspeed + dogspeed);
         friend = 1;
-        distance = distance - (firstfriendspeed + secondfriendspeed) * time;
-        count = count + 1;
     }
-    
+    distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+    count = count + 1;
 }
 
 Console.Write("Собака пробежит ");
