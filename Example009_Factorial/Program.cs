49462c60f87d3fb179968bd7001a2c586a857ed9
Stasensus <stanislav@kireev.ru>
2022-08-07 11:50:43 +0300

    Нахождение факториала

---
+++ Example009_Factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int fact = 1;
+int current = 1;
+while (current < number)
+{
+    current++;
+    fact = fact * current;
+}
+System.Console.WriteLine("Факториал " + number + " - " + fact);
