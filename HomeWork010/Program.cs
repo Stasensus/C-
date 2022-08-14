4478265da2d05dea4f195f3a855406979fafe111
Stasensus <stanislav@kireev.ru>
2022-08-14 16:01:48 +0300

    Задача №15 решена

--- HomeWork010/Program.cs
+++ HomeWork010/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result = number / 10 % 10;
-System.Console.WriteLine($"Вторая цифра : {result}");
+System.Console.WriteLine($"Вторая цифра: {result}");
 
---
+++ Homework015/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 6) System.Console.WriteLine("Вали на работу!");
+if (number > 7) System.Console.WriteLine("Где ты видел такой календарь, баран?");
+else System.Console.WriteLine("Можешь оставаться дома. Кайфуй!");
