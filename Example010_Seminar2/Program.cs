00895821f1db6bb5e175d009afd09bd9421d3ebe
Stasensus <stanislav@kireev.ru>
2022-08-14 14:51:02 +0300

    Решил все задачи перед семинаром

---
+++ Example010_Seminar2/Program.cs
@@ -0,0 +1,50 @@
+﻿// САМОСТОЯТЕЛЬНО РЕШЕННАЯ ЗАДАЧА ПЕРЕД СЕМИНАРОМ НОМЕР 1
+
+// int number = new Random().Next(100, 1000);
+// System.Console.WriteLine(number);
+
+// int firstDigit = number / 100;
+// int secondDigit = number % 100 / 10;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+
+// System.Console.WriteLine(result);
+
+// САМОСТОЯТЕЛЬНО РЕШЕННАЯ ЗАДАЧА ПЕРЕД СЕМИНАРОМ НОМЕР 2
+
+// System.Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber % secondNumber == 0) System.Console.WriteLine("Кратно");
+// else System.Console.WriteLine("Не кратно");
+
+// САМОСТОЯТЕЛЬНО РЕШЕННАЯ ПЕРЕД СЕМИНАРОМ ЗАДАЧА НОМЕР 3
+
+// System.Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber % 7 == 0)
+// {
+//     if (firstNumber % 23 == 0)
+//     System.Console.WriteLine("ДА!");
+//     else System.Console.WriteLine("НЕТ!");
+// }
+// else System.Console.WriteLine("НЕТ!");
+
+// СЕМИНАР
+
+// int number = new Random().Next(10, 100);
+// System.Console.WriteLine(number);
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit)   МОЙ ВАРИАНТ РЕШЕНИЯ
+// {
+//     System.Console.WriteLine(firstDigit);
+// }
+// else System.Console.WriteLine(secondDigit);
+
+// int maximum = firstDigit;   ДРУГОЙ ВАРИАНТ РЕШЕНИЯ С СЕМИНАРА
+// if (secondDigit > maximum) maximum = secondDigit;
+// System.Console.WriteLine(maximum);
+
