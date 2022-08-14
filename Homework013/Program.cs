d6878b09cb7968167b044df0ad550fc526ee28a5
Stasensus <stanislav@kireev.ru>
2022-08-14 16:13:21 +0300

    Задача №13 решена

---
+++ Homework013/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) 
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    string number2 = Convert.ToString(number);
+    System.Console.WriteLine($"Третья цифра - {number2[2]}");
+}
