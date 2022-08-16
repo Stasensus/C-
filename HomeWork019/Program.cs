1ce68f53cb3e0e2bf9e835ac41856460851f7347
Stasensus <stanislav@kireev.ru>
2022-08-17 00:18:11 +0300

    Задача решена

---
+++ HomeWork019/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое положительное пятизначное число");
+string number = (Console.ReadLine());
+string result = "XZ";
+for (int i = 0, j = 4; j >= i; i++, j--)
+{
+    if (number[i] != number[j])
+    {
+       result = "False";
+       break;
+    }
+    else result = "True";
+}
+System.Console.WriteLine(result);
