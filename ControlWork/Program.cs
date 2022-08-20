55f07bf1fac3dc3e70ca5b5d1eeba2b7fb7b1160
Stasensus <stanislav@kireev.ru>
2022-08-21 00:58:43 +0300

    Задача в целом решена, осталось побороть запятые, разделяющие пустые строки массива при выводе...

---
+++ ControlWork/Program.cs
@@ -0,0 +1,39 @@
+﻿// int[] array = new int [10];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 3000);
+//     Console.Write(array[i] + " ");
+// }
+
+
+
+
+// System.Console.Write("Введите количество элементов массива:");
+// int elementsCount = int.Parse(Console.ReadLine());
+// int [] array = new int[elementsCount];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write($"Введите элемент массива №{i}: ");
+//     array[i] = int.Parse(Console.ReadLine());
+// }
+// System.Console.WriteLine($"У вас получился массив [{String.Join("; ", array)}]");
+
+string[] array = new string[5];
+string str;
+Console.WriteLine("Введите строки: ");
+for (int i = 0; i < array.Length; i++)
+{
+    str = Convert.ToString(Console.ReadLine());
+    if (str.Length <= 3)
+    {
+        array[i] = str;
+    }
+}
+System.Console.WriteLine($"У вас получился массив [{String.Join(", ", array)}]");
+
+
+// string[] array2 = new string[5];
+// for (int i = 0; i < array2.Length; i++)
+// {
+//     if 
+// }
