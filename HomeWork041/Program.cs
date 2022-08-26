29f56440e76d5e55bd9432f9c8eb7ec44975d639
Stasensus <stanislav@kireev.ru>
2022-08-26 15:19:30 +0300

    задача решена

---
+++ HomeWork041/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Сколько планируете ввести чисел?");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+System.Console.WriteLine("Вводите, пожалуйста:");
+for (int i = 0; i < x; i++)
+{
+    int y = Convert.ToInt32(Console.ReadLine());
+    if (y > 0) count++;
+}
+System.Console.WriteLine($"Количество введенных положительных чисел: {count}");
