b10166b7ef44e041a75b2e161a7a2dc99a1046d8
Stasensus <stanislav@kireev.ru>
2022-08-28 15:51:18 +0300

    Исправил название массива

--- HomeWork050/Program.cs
+++ HomeWork050/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-int[,] array = new int[5,5];
-for (int i = 0; i < array.GetLength(0); i++)
+int[,] matrix = new int[5,5];
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        array[i,j] = new Random().Next(10);
-        Console.Write(array[i, j] + "\t");
+        matrix[i,j] = new Random().Next(10);
+        Console.Write(matrix[i, j] + "\t");
     }
     System.Console.WriteLine();
 }
@@ -15,8 +15,8 @@ int m = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите адрес столбца:");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if (m <= array.GetLength(0) && n <= array.GetLength(1)) 
+if (m <= matrix.GetLength(0) && n <= matrix.GetLength(1)) 
 {
-    System.Console.WriteLine($"По этому адресу находится элемент: {array[m,n]}");
+    System.Console.WriteLine($"По этому адресу находится элемент: {matrix[m,n]}");
 }
 else System.Console.WriteLine("Такого элемента нет");
