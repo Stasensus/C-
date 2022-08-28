4c92883c343dd66ebf0484f4b820636a9352d135
Stasensus <stanislav@kireev.ru>
2022-08-28 15:50:55 +0300

    Исправил название массива

--- HomeWork047/Program.cs
+++ HomeWork047/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-double[,] array = new double[3,4];
+double[,] matrix = new double[3,4];
 Random random = new Random();
-for (int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        array[i,j] = (random.NextDouble() * 2 - 1) * 10;
-        Console.Write("{0,6:F1}", array[i, j]);
+        matrix[i,j] = (random.NextDouble() * 2 - 1) * 10;
+        Console.Write("{0,6:F1}", matrix[i, j]);
     }
     System.Console.WriteLine();
 }
